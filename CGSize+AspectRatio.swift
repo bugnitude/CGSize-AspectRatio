6320a156b73a26ecb05ccc556dafb767d9c770e5
Kazumasa Kojima <kojima@bugnitude.com>
2017-06-14 00:18:51 +0900

    Add CGSize+AspectRatio.swift

---
+++ CGSize+AspectRatio.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+extension CGSize {
+	
+	func aspectFit(into size: CGSize) -> CGSize {
+		if self.width == 0.0 || self.height == 0.0 {
+			return self
+		}
+		
+		let widthRatio = size.width / self.width
+		let heightRatio = size.height / self.height
+		let aspectFitRatio = min(widthRatio, heightRatio)
+		return CGSize(width: self.width * aspectFitRatio, height: self.height * aspectFitRatio)
+	}
+
+	func aspectFill(into size: CGSize) -> CGSize {
+		if self.width == 0.0 || self.height == 0.0 {
+			return self
+		}
+
+		let widthRatio = size.width / self.width
+		let heightRatio = size.height / self.height
+		let aspectFitRatio = max(widthRatio, heightRatio)
+		return CGSize(width: self.width * aspectFitRatio, height: self.height * aspectFitRatio)
+	}
+	
+}
